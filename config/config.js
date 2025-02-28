d6a97891fd4997b6a9ace117c0687b39dd77687f
AkshAI-2030 <akshayarelli2030@gmail.com>
2025-02-28 13:11:32 +0530

    added config NODE_ENV

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: "postgres",
   },
-  development: {
+  production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
